2522bf773351325e33be847d6f49e45c87860be1
Alexander Garyaev <125agaryaev@frhsd.com>
2024-11-19 14:03:53 -0500

    working on it

--- receipt/src/App.tsx
+++ receipt/src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import jsonData from "./receipt1.json"
+import Item from "./Item";
 
-function App() {
+function App()
+{
+  var receipt = jsonData.receipt;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {
+      receipt.map(item => <Item item = {item}></Item>)
+    }
+
+    </>
   );
+
 }
+
+
 
 export default App;
---
+++ receipt/src/Item.tsx
@@ -0,0 +1,16 @@
+export default function Item({item}: {item: any})
+{
+
+
+
+
+
+
+
+
+    return(
+        <>
+        </>
+    )
+
+}
