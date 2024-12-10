df71ebab5747ba3b455ae62b086b2e3ce62901ed
Alexander Garyaev <125agaryaev@frhsd.com>
2024-12-10 11:45:04 -0500

    working on it

--- receipt/src/Item.tsx
+++ receipt/src/Item.tsx
@@ -5,26 +5,60 @@ export default function Item({ item }: { item:any})
             if (item.quantity===1)
             {
                 return (
-                    <div style={{backgroundColor:"green",display:"flex",justifyContent:"center",alignItems:"center", }} >
-                     {item.variety} cookie with a price of {item.price}</div>); }
-            else
-            {
-                return(
-                    <div style={{backgroundColor:"lightgreen",display:"flex",justifyContent:"center",alignItems:"center", }} >{item.quantity} {item.variety} cookies with a total price of{" "} {(item.price * item.quantity).toFixed(2)}</div>);
+                    <div style={{backgroundColor:"pink",display:"flex",justifyContent:"center",alignItems:"center", }} >
+                        <div style={{display:"flex",justifyContent:"left", width: 100, height: 50}}> <img src = "https://peanutbutterandjulie.com/wp-content/uploads/2023/12/Stack-of-Rainbow-cookies-sq.jpg"  alt = "Image"/></div>
+
+                    <h2>
+                        {item.variety} cookie with a price of {item.price}
+                    </h2>
+                       </div>
+
+                );
             }
-        }
-        else
-            if(item.type==="cake")
-            {
-                return(
-                    <div style={{backgroundColor:"pink",padding:"10px",margin:"10px",borderRadius:"5px", }} >A {item.shape} {item.flavor} cake {item.shape === "round" ? ` with a radius of ${item.radius}` : ` with dimensions of ${item.length}x${item.width}`} , priced at {item.price}. Message: "{item.message}".</div>);
+            else {
+                return (
+                    <div style={{
+                        backgroundColor: "pink",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}>
+                        <div style={{display: "flex", justifyContent: "left", width: 100, height: 50}}><img
+                            src="https://www.simplyrecipes.com/thmb/2g1IOS6TYdG_bmyg0hyf0b6RKGY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Oatmeal-Raisin-Cookies-LEAD-15-2251dafb1a554aafaedf42e797bf2fd0.jpg"
+                            alt="Image"/></div>
+                        <h2>
+                            {item.quantity} {item.variety} cookies with a total price
+                            of{" "} {(item.price * item.quantity).toFixed(2)}
+                        </h2>
+                    </div>);
+
             }
-            else
-                if (item.type==="doughnut")
-                {
-                    return(
-                        <div style={{backgroundColor:"yellow",padding:"10px",margin:"10px",borderRadius:"5px", }} >{item.quantity} {item.flavor} doughnuts for a total of{" "} {(item.price * parseInt(item.quantity)).toFixed(2)}.</div>);
-                }
-                return(
-                    <div style={{backgroundColor:"gray",padding:"10px",margin:"10px" }}>item type.</div>);
+        } else if (item.type === "cake") {
+        return (
+            <div style={{backgroundColor: "pink", padding: "10px", margin: "10px", borderRadius: "5px",}}>
+            <div style={{display: "flex", justifyContent: "left", width: 100, height: 50}}><img
+                            src="https://www.theflouredtable.com/wp-content/uploads/2022/03/Chocotorta-15-500x500.jpg"
+                            alt="Image"/></div>
+                        <h2>
+                            A {item.shape} {item.flavor} cake {item.shape === "round" ? ` with a radius of ${item.radius}` : ` with dimensions of ${item.length}x${item.width}`} ,
+                            priced at {item.price}. Message: "{item.message}".
+                        </h2>
+                    </div>);
+            }
+            else if (item.type === "doughnut") {
+                return (
+                    <div style={{backgroundColor: "pink", padding: "10px", margin: "10px", borderRadius: "5px",}}>
+                        <div style={{display: "flex", justifyContent: "left", width: 100, height: 50}}><img
+                            src="https://sugarpursuit.com/wp-content/uploads/2023/02/Boston-cream-donuts-thumnail.jpg"
+                            alt="Image"/></div>
+                        <h2>
+                            {item.quantity} {item.flavor} doughnuts for a total
+                            of{" "} {(item.price * parseInt(item.quantity)).toFixed(2)}.
+                        </h2>
+                    </div>);
+            }
+    return (
+        <div style={{backgroundColor: "pink", padding: "10px", margin: "10px"}}>item type.</div>);
+
+
 }
